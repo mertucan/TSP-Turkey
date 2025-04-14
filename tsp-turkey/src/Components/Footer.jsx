a61181711b2698f827d650a55083b331738f7b09
Mert Uçan <mertucan12@gmail.com>
2025-04-14 10:31:59 +0300

    Refactor code structure for improved readability and maintainability

--- tsp-turkey/src/Components/Footer.jsx
+++ tsp-turkey/src/Components/Footer.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-        <p>Yapan Kişi: Mert Uçan</p>
+    <footer className="site-footer">
+      <div className="footer-container">
+        <div className="developer-info">
+          <p>Designed By: Mert Uçan</p>
+          <a 
+            href="https://github.com/mertucan" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="github-link"
+            title="GitHub Profilini Ziyaret Et"
+          >
+            <img 
+              src="/img/github.png" 
+              alt="GitHub" 
+              className="github-icon" 
+              width="24" 
+              height="24" 
+            />
+          </a>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
